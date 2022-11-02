dea78426efabb3ed745475b379bc74bbc23d1378
Kayleigh Watts <kwatts949@gmail.com>
2022-11-02 15:18:41 +0000

    Update database connection

--- app.rb
+++ app.rb
@@ -5,8 +5,6 @@ require_relative 'lib/database_connection'
 require_relative 'lib/album_repository'
 require_relative 'lib/artist_repository'
 
-DatabaseConnection.connect
-
 class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
